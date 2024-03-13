3f1345f2cdb57e3ba58fbcfe0c3d9d2874cdfcd3
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-03-12 21:07:25 -0300

    .
    .

--- hooks/useEffect-batisti/src/App.jsx
+++ hooks/useEffect-batisti/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [countb, setCountb] = useState(10);
+
+  useEffect (()=> {
+    console.log('Roda a cada Renderização')
+  })
+
+  useEffect (() => {
+    console.log('So roda quando o valor de algum estado selecionado for incrementado')
+  }, [count])
+
+
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button onClick={() => setCount((prevCount) => count + 1)}>Renderinzar</button>
+      <p>{count}</p>
+    </div>
+
+
+    <div>
+      <button onClick={() => setCountb((prevCount) => countb + 1)}>Renderinzar</button>
+      <p>{countb}</p>
+    </div>
+
     </>
+
   )
 }
 
 export default App
+
