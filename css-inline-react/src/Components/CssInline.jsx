29d7d29b8303b361a3fcdd92804e8c0b22aae941
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-03-22 13:07:08 -0300

    subindo atividade css inline react

---
+++ css-inline-react/src/Components/CssInline.jsx
@@ -0,0 +1,21 @@
+const CssInline = () => {
+
+
+    return (
+
+        <div>
+            <h2 style={{color: 'red'}}>teste Css Inline</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, quibusdam pariatur expedita obcaecati num
+                quam odio reiciendis
+                , dolorum corrupti minima incidunt consequatur itaq
+                ue doloremque magnam. Ipsam temporibus ex nobis qui
+                nemo!</p>
+            
+            <button style={{border: '1px solid red'}}>Enviar</button>
+
+        </div>
+    )
+}
+
+
+export default CssInline;
