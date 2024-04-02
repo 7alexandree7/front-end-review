320fec60af0583309117beea9a1369bf85639d25
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-04-02 17:44:26 -0300

    secret word

---
+++ SecretWord/src/Components/StartScreen.jsx
@@ -0,0 +1,13 @@
+const StartScreen = ({}) => {
+
+    return (
+        <div>
+            <h1>Secret Word</h1>
+            <p>Clique no botão para começar a jogar</p>
+            <button>Começar o jogo</button>
+        </div>
+    )
+}
+
+
+export default StartScreen;
---
+++ SecretWord/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Capõ", "Pneu", "Antena"],
+    fruta: ["banana", "Maçã", "Pera", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    progamação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
