ea7a6a648a5411d9bb3a69185ab6c947197e263c
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-04-02 17:51:38 -0300

    ..

--- SecretWord/src/Components/StartScreen.jsx
+++ SecretWord/src/Components/StartScreen.jsx
@@ -1,10 +1,13 @@
+import './StartScreen.css'
+
 const StartScreen = ({}) => {
 
     return (
-        <div>
+        <div className>
             <h1>Secret Word</h1>
             <p>Clique no botão para começar a jogar</p>
             <button>Começar o jogo</button>
+            <p></p>
         </div>
     )
 }
