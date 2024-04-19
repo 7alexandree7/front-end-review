2a01e08ce7717f3cc3c90db67796cd62b590a972
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-04-19 18:53:22 -0300

    continuidade ao projeto de rotas

--- React-Router/React-Router-Batisti-01/src/App.jsx
+++ React-Router/React-Router-Batisti-01/src/App.jsx
@@ -3,32 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+//1- config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>
+      <h2>React Router</h2>
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
---
+++ React-Router/React-Router-Batisti-01/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = ({}) => {
+
+    return (
+        <div>
+            <h4>Essa é minha pagina Home</h4>
+            <button>Pagina4</button>
+        </div>
+    )
+}
+
+
+export default Home
