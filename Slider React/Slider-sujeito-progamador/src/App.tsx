8948df282792e670fb1a722e1703ca94b8775401
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-07 11:41:36 -0300

    sdf

---
+++ Slider React/Slider-sujeito-progamador/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div className='App'>
+      <div className='container'>
+        <h2>Slider com React Js</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
