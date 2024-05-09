52c552df0e8545131ad53b0686941ca2168288aa
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-09 13:27:29 -0300

    1 break

--- Projetos React/projeto-cep-sujeito-progamador/src/App.jsx
+++ Projetos React/projeto-cep-sujeito-progamador/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {FiSearch} from 'react-icons/fi'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,28 +8,31 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className='container'>
+
+      <h1 className='title'>Bucasdor De Cep</h1>
+
+      <div className='container-input'>
+
+        <input type="text" name="" id="" placeholder='Digite seu CEP...'/>
+        <button className='buttonSearch'> <FiSearch size={25} color='#000'/> </button>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+      <main className='main'>
+
+        <h2>CEP: 42.700-600</h2>
+
+        <span>Rua: alguma rua</span>
+        <span>Complemento: algum complemento</span>
+        <span>Bairro: algum bairro</span>
+        <span>Campo grande - ms</span>
+
+      </main>
+
+    </div>
   )
 }
 
