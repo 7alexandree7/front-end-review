61c8180167f61c53a00885a621b9626c1329ed5e
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-16 18:13:45 -0300

    finalizando o exercicio

--- hooks/UseEffect/UseEffect-front-end-benigners/src/App.jsx
+++ hooks/UseEffect/UseEffect-front-end-benigners/src/App.jsx
@@ -1,10 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [secondCount, setSecondCount] = useState(0);
+
+
+  //Vai renderizar sempre
+  useEffect(() => {
+    console.log('executa sempre');
+  })
+
+
+  //vai renderizar so uma vez quando a pagina for carregada
+  useEffect(() => {
+    console.log('executa na primeira vez que a pagina é carregada');
+  }, [])
+
+
+  //Rendereiza sempre que um estado especifico é alterado
+  useEffect(() => {
+    console.log('executa sempre que um estado especifico é alterado');
+  }, [secondCount])
+
+
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      console.log(`O count foi executado ${count} vezes`)
+    },2000) 
+
+    return () => {
+      clearInterval(timer)
+    }
+  },[count])
+
+
 
   return (
     <div>
@@ -14,6 +48,13 @@ function App() {
         <p>foi rendereizado {count}</p>
         <button onClick={() => setCount((count) => count + 1)}>Render</button>
       </div>
+
+
+      <div className='card'>
+        <p>foi rendereizado {secondCount}</p>
+        <button onClick={() => setSecondCount((secondCount) => secondCount + 1)}>Render</button>
+      </div>
+
     </div>
   )
 }
