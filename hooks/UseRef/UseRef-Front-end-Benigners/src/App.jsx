307bd0540a977b16d8fde114a9481aa473d3138b
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-20 21:39:47 -0300

    x

--- hooks/UseRef/UseRef-Front-end-Benigners/src/App.jsx
+++ hooks/UseRef/UseRef-Front-end-Benigners/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    countRef.current = count
+    console.log(countRef.current);
+  },[count])
+
+
+  useEffect(() => {
+    buttonRef.current.click();
+  },[])
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1>UseRef Front-end benigners</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+
+        <h2>Contador Original: {count}</h2>
+        <h2>Contador por referencia {countRef.current}</h2>
+
+        <button ref={buttonRef} onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
