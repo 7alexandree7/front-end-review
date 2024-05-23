92ffe197de9eba4ee27869d7187a0ff78939463e
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-23 13:41:35 -0300

    continuação github finder

--- Projetos React/github-finder/src/App.tsx
+++ Projetos React/github-finder/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
+import { Outlet } from "react-router-dom"
+
 
 function App() {
 
   return (
     <div className='App'>
+
+      <h1>GitHub Finder</h1>
+      <Outlet />
 
     </div>
   )
