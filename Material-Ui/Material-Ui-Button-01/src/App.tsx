f9604f89a69f1f8c766f71592eeb810a40b8f21b
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-28 21:57:53 -0300

    finalizando 1 atividade

---
+++ Material-Ui/Material-Ui-Button-01/src/App.tsx
@@ -0,0 +1,19 @@
+import BasicButtons from './Components/BasicButtons'
+import './App.css'
+
+function App() {
+
+
+  
+
+  return (
+    
+    <div>
+      <BasicButtons />
+      <h2>Button Material Ui</h2>
+      
+    </div>
+  )
+}
+
+export default App
