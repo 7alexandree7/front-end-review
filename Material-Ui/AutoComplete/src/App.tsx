daf76c87a54e5bada85a675638a6854952f6e5b9
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-05-31 12:52:20 -0300

    i

---
+++ Material-Ui/AutoComplete/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import AutoCompleteCountry from './Components/AutoCompleteCountry'
+import AutoCompleteCheckBox from './Components/AutoCompleteCheckBox'
+import AutoCompleteTxt from './Components/AutoCompleteTxt'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <AutoCompleteCountry />
+      <AutoCompleteCheckBox />
+      <AutoCompleteTxt />
+    </div>
+
+  )
+}
+
+export default App
