d515126590bddfd29994e2a9779884673b207622
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-04 11:07:22 -0300

    d

--- hooks/UseState/DevJunior-Alves/src/App.tsx
+++ hooks/UseState/DevJunior-Alves/src/App.tsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState('');
+  const [people, setPeople] =  useState({ name: 'Alexandre', address: 'Rua do futuro' })
+  console.log(people)
+
 
   return (
-    <>
+
+    <div>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        {!!name && <p>{name}</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <input type="text" value={people.name}  onChange={(e) => setPeople((prev) => ({...prev, name: e.target.value}))}/>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
+
   )
 }
 
