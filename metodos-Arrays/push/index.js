3ab8a3cc49e7c3ee7cdbf9870818d734b768dcf9
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-19 15:17:22 -0300

    exercicio push simple

---
+++ metodos-Arrays/push/index.js
@@ -0,0 +1,8 @@
+const fruits = ['banana', 'maçã', 'uva', 'pera'];
+fruits.push('ultima fruta');
+
+const numeros = [1, 2, 3, 4, 5, 6];
+numeros.push('ultimo numero');
+
+console.log(fruits);
+console.log(numeros)
