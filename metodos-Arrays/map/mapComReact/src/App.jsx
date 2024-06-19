65316caad482ea358dc889f00db25a50dcccf027
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-19 12:34:03 -0300

    i

--- metodos-Arrays/map/mapComReact/src/App.jsx
+++ metodos-Arrays/map/mapComReact/src/App.jsx
@@ -6,28 +6,57 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const users = [
+
+    {
+      name: 'alexandre',
+      address: 'rua x',
+      age: 21,
+      isAdimin: false
+    },
+
+    {
+      name: 'guilherme',
+      address: 'rua e',
+      age: 25,
+      isAdimin: false
+    },
+
+    {
+      name: 'adriana',
+      address: 'rua sdf',
+      age: 53,
+      isAdimin: true
+    },
+
+    {
+      name: 'alessandro',
+      address: 'rua x',
+      age: 48,
+      isAdimin: false
+    },
+
+    {
+      name: 'arnaldinho',
+      address: 'rua yjukx',
+      age: 55,
+      isAdimin: true
+    },
+
+  ]
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {users.map((user, index) => {
+        return (
+          <div key={index}>
+            <p>{user.name}</p>
+            <p>{user.address}</p>
+            <p>{user.age}</p>
+            <p>{user.isAdimin}</p>
+          </div>
+        )
+      })}
     </>
   )
 }
