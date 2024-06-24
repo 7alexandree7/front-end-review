d57a41cc58919a783845c93a864c641e647a99d6
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-23 21:42:50 -0300

    h

---
+++ metodos-Arrays/shift/index.js
@@ -0,0 +1,16 @@
+const numbers = [10, 15, 20, 25, 30, 35];
+const removerElements = numbers.shift();
+
+console.log(numbers);
+console.log(removerElements);
+
+
+
+const times = ['são paulo', 'botafogo', 'vitoria', 'bahia', 'internacional', 'gremio'];
+
+const removerPrimeiroTime = times.shift();
+const removerUltimoTime = times.pop();
+
+console.log(times);
+console.log(removerPrimeiroTime);
+console.log(removerUltimoTime);
---
+++ metodos-Arrays/unshift/index.js
@@ -0,0 +1,5 @@
+const numbers = [10, 20, 30, 40, 50];
+const totalNumbers = numbers.unshift(0, 5);
+
+console.log(numbers);
+console.log(totalNumbers);
