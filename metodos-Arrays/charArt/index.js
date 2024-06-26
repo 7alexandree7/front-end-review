da25dd18b327f7ecb609836f19e7cf0273810456
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-26 19:02:06 -0300

    sd

---
+++ metodos-Arrays/charArt/index.js
@@ -0,0 +1,8 @@
+var anyString = "Brave new world";
+
+console.log("A letra no índice 0 é '" + anyString.charAt(0) + "'");
+console.log("A letra no índice 1 é '" + anyString.charAt(1) + "'");
+console.log("A letra no índice 2 é '" + anyString.charAt(2) + "'");
+console.log("A letra no índice 3 é '" + anyString.charAt(3) + "'");
+console.log("A letra no índice 4 é '" + anyString.charAt(4) + "'");
+console.log("A letra no índice 99 é '" + anyString.charAt(999) + "'");
