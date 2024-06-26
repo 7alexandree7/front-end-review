d7043aacbcebb418e991938aa559f0692e0e292c
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-06-26 12:32:10 -0300

    sd

---
+++ metodos-Arrays/length/index.js
@@ -0,0 +1,10 @@
+const texto1 = 'primeiro texto';
+const texto2 = ['alexandre', 'jose', 'pedro', 'mycon']
+
+console.log(texto1.length);
+console.log(texto2.length)
+
+
+for (let i = 0; i < texto2.length; i++) {
+    console.log(texto2[i])
+}
