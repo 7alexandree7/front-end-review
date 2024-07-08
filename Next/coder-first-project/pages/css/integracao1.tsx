c763c59c85ecf5ee8c541f442534dbf56d67d08e
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-07-08 16:33:23 -0300

    as

---
+++ Next/coder-first-project/pages/css/integracao1.tsx
@@ -0,0 +1,11 @@
+export default function integracao1() {
+
+    return (
+        <>
+            <div className="vermelha"><h1>Cor em vermelho</h1></div>
+            <div className="azul"><h1>Cor em Azul</h1></div>
+            <div className="branco"><h1>Cor em branco</h1></div>
+        </>
+    )
+}
+
---
+++ Next/coder-first-project/pages/css/integracao2.tsx
@@ -0,0 +1,17 @@
+import styles from "./integracao2.module.css"
+
+
+
+export default function integracao2() {
+
+    return (
+       
+            <div id={styles.integracao}>
+                <div className={styles.vermelha}><h1>Cor em vermelhoo</h1></div>
+                <div className={styles.azul}><h1>Cor em Azul</h1></div>
+                <div className={styles.branco}><h1>Cor em branco</h1></div>
+            </div>
+
+        
+    )
+}
