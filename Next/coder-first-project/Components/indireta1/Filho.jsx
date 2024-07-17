eae6f9dc64581e7a5be732f35db078603384d77a
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-07-17 17:38:26 -0300

    s

---
+++ Next/coder-first-project/Components/indireta1/Filho.jsx
@@ -0,0 +1,8 @@
+export default function Filho () {
+
+    return (
+        <div>
+            <h1>Componente Filho</h1>
+        </div>
+    )
+}
---
+++ Next/coder-first-project/Components/indireta1/Pai.jsx
@@ -0,0 +1,8 @@
+export default function Pai () {
+
+    return (
+        <div>
+             <Filho/>
+        </div>
+    )
+}
